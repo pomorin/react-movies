c8c2f2ddd472d1ec70ff7d47e1027c175f459c20
DESKTOP-DG3BGOU\Pierrot <pmorin@gmail.com>
2019-02-24 11:16:35 -0500

    creating master branch

---
+++ src/components/elements/LoadMoreBtn/LoadMoreBtn.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './LoadMoreBtn.css';
+
+const LoadMoreBtn = (props) => (
+  <div className="rmdb-loadmorebtn" onClick={props.onClick}>
+    <p>{props.text}</p>
+  </div>
+)
+
+export default LoadMoreBtn;
---
+++ src/components/elements/Spinner/Spinner.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => (
+  <div>
+    <div className="loader"></div>
+  </div>
+)
+
+export default Spinner;
